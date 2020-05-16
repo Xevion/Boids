39af1b95f757586576d6cf636643ca54914e9527
Xevion <xevioni@yandex.com>
2020-05-16 09:19:27 -0500

    add all files, project init

---
+++ Assets/BoidController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using KNN;
+using KNN.Jobs;
+
+public class BoidController : MonoBehaviour {
+    public int BoidCount = 1000;
+    public int BoidGroupRange = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# boids
+
+Another 2D Boids Project
+
+# About
+
+A experiment to see how well I can program Boids.
